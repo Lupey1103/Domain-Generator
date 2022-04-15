752c0dac8aef0751027317b25f7b04f225daa237
Lupey1103 <luperon.christian95@gmail.com>
2022-04-15 05:09:11 +0000

    Domains for days

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let determiner = ["the", "a", "my", "many"];
+  let bands = ["slipknot", "lornashore", "avengedsevenfold", "chelseagrin"];
+  let domain = [".com", ".net", ".net", ".org"];
+
+  function newDomain() {
+    document.getElementById("domain").innerHTML =
+      determiner[Math.floor(Math.random() * determiner.length)] +
+      bands[Math.floor(Math.random() * bands.length)] +
+      domain[Math.floor(Math.random() * domain.length)];
+  }
+
+  document.getElementById("myBtn").addEventListener("click", newDomain);
+
+  console.log("Make Your Websites Now!");
 };
